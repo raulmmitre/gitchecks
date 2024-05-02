9dacfad115b73cf872d8fd5b64a2752631e6dbee
alumnoT <alumnoT@INF1-12>
2024-05-02 19:51:41 +0300

    Este es el primer commit del proyecto EjercicioGit.

---
+++ EjercicioGit/src/identificacion/Identificador.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package identificacion;
+
+/**
+ * 
+ */
+public class Identificador {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
